3617a88cfdd45c775eaa10c10145c0f13f6a55d0
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2024-12-07 17:57:32 +0100

    add summary section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import React from "react";
 import Icons from "./Icons";
+import Summary from "./Summary";
 
 function Main() {
 	return (
 		<div>
 			<Icons />
+			<Summary />
 		</div>
 	);
 }
