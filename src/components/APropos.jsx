2cb98ccfa569b37d7e27ef5336d642672ae8657c
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2024-12-07 18:26:53 +0100

    Correctione scape text

--- src/components/APropos.jsx
+++ src/components/APropos.jsx
@@ -12,11 +12,12 @@ const APropos = () => {
 				<p className="font-poppins ">
 					There are many variations of passages of Lorem Ipsum available, but
 					the majority have suffered alteration in some form, by injected
-					humour, or randomised words which don't look even slightly believable.
-					If you are going to use a passage of Lorem Ipsum, you need to be sure
-					there isn't anything embarrassing hidden in the middle of text. All
-					the Lorem Ipsum generators on the Internet tend to repeat predefined
-					chunks as necessary, making this the first true generator on the.
+					humour, or randomised words which don&#39;t look even slightly
+					believable. If you are going to use a passage of Lorem Ipsum, you need
+					to be sure there isn&#39;t anything embarrassing hidden in the middle
+					of text. All the Lorem Ipsum generators on the Internet tend to repeat
+					predefined chunks as necessary, making this the first true generator
+					on the.
 				</p>
 			</div>
 		</div>
