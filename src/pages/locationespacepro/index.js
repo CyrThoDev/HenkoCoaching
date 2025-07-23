ca02a486d71bd8ce407b4899087edc15a2a6f615
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2025-07-23 14:52:20 +0200

    correct escape

--- src/pages/locationespacepro/index.js
+++ src/pages/locationespacepro/index.js
@@ -88,7 +88,7 @@ function LocationEspacePro({ seolocation, espacepro }) {
 
 				<div className="flex flex-col gap-0">
 					<h2 className="relative flex flex-col text-lg md:text-2xl font-tanker px-10 lg:px-20">
-						<span className="text-3xl z-10">Je souhaite avoir plus d'informations</span>
+						<span className="text-3xl z-10">Je souhaite avoir plus d&#39;informations</span>
 						<div className="w-[12rem] h-3 bg-sand -mt-3 -z-1" />
 					</h2>
 					<Formulaire
